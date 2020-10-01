4c2381d7cdcae51dd09f8aa1319adff21b9bd80b
lorena <lorena.nunziato@aluno.ifsp.edu.br>
2020-09-30 21:59:31 -0300

    Pronto

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+var num1 = prompt("Informe o primeiro número:");
+var num2 = prompt("Informe o segundo número:");
+var int1 = parseInt(num1);
+var int2 = parseInt(num2);
+var soma = int1 + int2;
+console.log(soma);
+alert(soma);
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <script src = "Calculadora.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
